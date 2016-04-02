9fec3b1e1f1d4f69a1b73b98cb099b16f54b407b
mocchi <boom.boom.planet@gmail.com>
2016-04-02 21:35:25 +0900

    add foot-bar field

--- resources/public/js/writing-utensils.js
+++ resources/public/js/writing-utensils.js
@@ -1,5 +1,7 @@
 (function (){
     var borwserActions = {};
+    var FOOT_BAR_MODE_AT_SHOW_VIEW = 'show-view';
+    var subView = {};
 
     document.addEventListener('DOMContentLoaded', init, false);
 
@@ -8,6 +10,14 @@
         var headersAnchers = document.querySelectorAll('header > nav > ul > li > a');
         for(var i = 0; i < headersAnchers.length; i++) {
             headersAnchers[i].onclick = browserActionDispatcher;
+        }
+        // note button
+        var showViewButton = document.getElementById('show-view-button');
+        showViewButton.onclick = showView;
+        // subview
+        var subViews = document.querySelectorAll('.browser-action');
+        for(var i = 0; 0 < subViews.length; i++){
+            subView[subViews[i].getAttribute('id')] = subViews[i];
         }
     }
 
@@ -19,8 +29,27 @@
         }
     }
 
+    function showView() {
+        var note = subView['note'];
+        var footBar = subView['foot-bar'];
+        var reopenButton = document.createElement('button');
+        reopenButton.appendChild(document.createTextNode('exit'));
+        // footBar
+        footBar.innerHTML = '';//wip        
+        note.style.display = 'none';
+        footBar.dataset.mode = FOOT_BAR_MODE_AT_SHOW_VIEW;
+        footBar.innerHTML = 'exit';
+        footBar.onclick = function () {
+            note.style.display = 'block';
+            footBar.style.display = 'none';
+        }
+
+        footBar.style.display = 'block';
+        
+    }
+
     function openNoteViewer() {
-        var note = document.getElementById('note');
+        var note = subView['note'];
         note.style.display = 'block';
         titleEditingArea.focus();
     }
@@ -30,7 +59,7 @@
     };
 
     borwserActions['#logout'] = function() {
-
+        location.href = '/logout';
     };
 
 })();
